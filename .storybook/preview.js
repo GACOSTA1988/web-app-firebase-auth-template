e9a3a08505d5312934684d536f2178dd2591879d
GACOSTA1988 <georgeacostar@gmail.com>
2021-10-03 11:56:28 -0700

    adds additional storybook configs

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,40 @@
+import { MINIMAL_VIEWPORTS } from '@storybook/addon-viewport'
+
+const customViewports = {
+  iPhone8: {
+    name: 'iPhone 8',
+    styles: {
+      width: '375px',
+      height: '667px',
+    },
+    type: 'mobile',
+  },
+  ultrawide: {
+    name: 'ultra-wide desktop',
+    styles: {
+      width: '1920px',
+      height: '1080px',
+    },
+  },
+}
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  viewport: {
+    viewports: {
+      ...MINIMAL_VIEWPORTS,
+      ...customViewports,
+    },
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
+    },
+  },
+  options: {
+    storySort: {
+      method: 'alphabetical',
     },
   },
 }
