7b6726bfd7039c0feb286ee84614843797823eee
GACOSTA1988 <georgeacostar@gmail.com>
2021-10-03 14:50:16 -0700

    updates login screen

--- src/ui/modules/login/pages/login.tsx
+++ src/ui/modules/login/pages/login.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Button } from "@material-ui/core"
+import { useHistory } from 'react-router-dom';
 
 const LoginPage: React.FunctionComponent = () => {
-
+  const history = useHistory();
+  const handleClick = () => {
+    alert('clicked')
+    history.push("/");
+  }
     return (
-       <div>Login page</div>
+       <div>Login page        
+         <Button
+          color="primary"
+          onClick={handleClick}
+          >
+          Login
+        </Button></div>
     );
 }
 
